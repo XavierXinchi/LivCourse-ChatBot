678ebccc550af57bbf307d06a885536e0df8fd5e
XavierXinchi <xinchi.shi20@gmail.com>
2024-03-05 23:29:58 +0000

    formate the code

--- gen_kg.py
+++ gen_kg.py
@@ -2,6 +2,8 @@ from py2neo import Graph
 import json
 from tqdm import tqdm
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 class CreateKG():
 
@@ -10,13 +12,13 @@ class CreateKG():
         self.graph = Graph(uri)
 
         if not data_path or data_path == '':
-            raise Exception("数据集地址为空")
+            raise Exception("Dataset address is empty")
         if not os.path.exists(data_path):
-            raise Exception("数据集不存在")
+            raise Exception("Dataset does not exist")
         self.data_path = data_path
 
     def saveEntity(self, label, data):
-        print("\n写入实体：", label)
+        print("\nWrite in entity: ", label)
         for item in tqdm(data, ncols=80):
             try:
                 property = []
@@ -31,7 +33,7 @@ class CreateKG():
                 print("Error saving entity:", e)
 
     def saveRelation(self, s_label, e_label, relation_type, relations):
-        print(f"\n写入关系：{relation_type}")
+        print(f"\nWrite in relationships: {relation_type}")
         for relation in tqdm(relations):
             try:
                 s_name = relation["s_name"].replace("'", "\\'")
@@ -45,52 +47,52 @@ class CreateKG():
                 print(f"Error saving relation {relation_type}: {e}")
 
     def init(self):
-        # 初始化实体和关系的空列表
+        # Initialize an empty list of entities and relationships
         universities, degrees, years, compulsoryModules, optionalModules = [], [], [], [], []
         universityDegreeRelations, degreeYearRelations, yearCompulsoryModuleRelations, yearOptionalModuleRelations = [], [], [], []
 
         with open(self.data_path, 'r', encoding='utf8') as f:
             data = json.load(f)
 
-            # 处理大学实体
+            # append university entities
             universities.append({"name": data["university"]})
 
             for degree in data["degrees"]:
-                # 处理学位实体及其与大学的关系
+                # append degree entities and their relationship with the University
                 degrees.append({"name": degree["degree_name"]})
                 universityDegreeRelations.append({"s_name": data["university"], "e_name": degree["degree_name"]})
 
                 for year, details in degree["courses"].items():
-                    # 处理年级实体及其与学位的关系
+                    # append year entities and their relationship to degrees
                     yearName = f"{degree['degree_name']} {year.capitalize()}"
                     years.append({"name": yearName})
                     degreeYearRelations.append({"s_name": degree["degree_name"], "e_name": yearName})
 
-                    # 处理必修模块实体及其与年级的关系
+                    # append compulsory module entities and their relationship to year entity
                     for module in details.get("compulsory_modules", []):
                         moduleCode = module["module_code"]
                         compulsoryModules.append({
                             "name": module["module_name"],
-                            "code": moduleCode,  # 假设使用 code 作为唯一标识符
+                            "code": moduleCode,  # Assuming code is used as a unique identifier
                             "credits": module["credits"],
                             "semester": module["semester"],
                             "desc": module["desc"]
                         })
                         yearCompulsoryModuleRelations.append({"s_name": yearName, "e_name": moduleCode})
 
-                    # 处理选修模块实体及其与年级的关系
+                    # append optional module entities and their relationship to year entity
                     for module in details.get("optional_modules", []):
                         moduleCode = module["module_code"]
                         optionalModules.append({
                             "name": module["module_name"],
-                            "code": moduleCode,  # 假设使用 code 作为唯一标识符
+                            "code": moduleCode,  # Assuming code is used as a unique identifier
                             "credits": module["credits"],
                             "semester": module["semester"],
                             "desc": module["desc"]
                         })
                         yearOptionalModuleRelations.append({"s_name": yearName, "e_name": moduleCode})
 
-        # 写入实体和关系
+        # Write in entities and relations
         self.saveEntity("University", universities)
         self.saveEntity("Degree", degrees)
         self.saveEntity("Year", years)
@@ -104,9 +106,9 @@ class CreateKG():
         
 if __name__ == '__main__':
     kg_host = "127.0.0.1"
-    kg_port = 7687 # 7689
-    kg_user = "neo4j"
-    kg_password = "SXc2002627SXc"
+    kg_port = 7687 # change to your own kg port
+    kg_user = os.getenv('NEO4J_USERNAME')
+    kg_password = os.getenv('NEO4J_PASSWORD')
     data_path = "./dataset/dataset.json"
     kg = CreateKG(kg_host, kg_port, kg_user, kg_password, data_path)
     kg.init()
